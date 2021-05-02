9cfbf81e1e6d98f8da50f8bf9ef8b7853378519d
Catalina Garrido <cpgarrido@uc.cl>
2021-05-02 01:08:34 -0400

    db.create

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 #data base
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/DATABASE_URL'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_SORT_KEYS'] = False
 
