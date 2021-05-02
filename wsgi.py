fe3c2ff3c620c113e5f1234cf534db1424c089c3
Catalina Garrido <cpgarrido@uc.cl>
2021-05-02 00:29:45 -0400

    db.create

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app import app, db
 ######################################  R U N   S E R V E R  #####################################
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
